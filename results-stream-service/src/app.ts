46b72ea2ecab6e90f4f46ef10b464af879f5bba8
Antti Pohjola <summeli@summeli.com>
2021-10-02 15:58:59 +0300

    send message to all clients

--- results-stream-service/src/app.ts
+++ results-stream-service/src/app.ts
@@ -13,11 +13,14 @@ const wss = new WebSocket.Server({ server });
 wss.on('connection', (ws: WebSocket) => {
 
     //connection is up, let's add a simple simple event
-    ws.on('message', (message: string) => {
-
-        //log the received message and send it back to the client
-        console.log('received: %s', message);
-        ws.send(`Hello, you sent -> ${message}`);
+    ws.on('message', function incoming(data) {
+      console.log('received: %s', data);
+      //send all data to every client connected to the server
+      wss.clients.forEach(function each(client) {
+        if (client !== ws && client.readyState === WebSocket.OPEN) {
+          client.send(data);
+        }
+      });
     });
 
     //send immediatly a feedback to the incoming connection    
